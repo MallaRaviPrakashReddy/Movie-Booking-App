cb76a407f88d04d825a236b3b67a55e5648e975e
Ravi Prakash <65698776+MallaRaviPrakashReddy@users.noreply.github.com>
2022-07-27 23:45:28 +0530

    checkpoint-3

--- server.js
+++ server.js
@@ -1,59 +1,40 @@
-// We would load all libraries like express and CORS
-const express = require("express");
-const cors = require("cors");
+//This file is entry point of node application.
+//We will be handling movies related WEB API end pnd points here using http module
+//Later on for maintainance purpose; 
+//we will be moving these entry points to routes folder using express
 
-// make express object 
-const app = express();
-app.use(express.json());
 
-// use the CORS object 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
+//Here we need to handle WEB API End points initially
 
-app.use(cors(corsOptions));
+//---  GET /movies
+//---  GET /genres
+//---  GET /artists
 
-// will create a mongoose object and connect to it.
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+//We also will have to work with URL parameters; but we will deal 
+//with those parameters directly using Express 
 
-// load the routes for movie 
-require("./app/routes/movie.routes")(app);
+const http = require('http');
 
-// load the routes for genre
-require("./app/routes/genre.routes")(app);
-
-// load the routes for artist
-require("./app/routes/artist.routes")(app);
-
-// load the routes for users
-require("./app/routes/user.routes")(app);
-
-// set up a default route for / 
-app.get("/", (req, res) => {
-  res.json({ message: "Movie booking application" });
+const server = http.createServer((request, response) => {
+    if (request.url == '/movies') {
+        if (request.method == 'GET') {
+            response.write("All Movies Data in JSON format from Mongo DB");
+        } 
+    } 
+    else if (request.url == '/genres') {
+        if (request.method == 'GET') {
+            response.write("All Genres Data in JSON format from Mongo DB");
+        }
+    } else if (request.url == '/artists') {
+        if (request.method == 'GET') {
+            response.write("All Artists Data in JSON format from Mongo DB");
+        }
+    }
+    response.end();
 });
 
+//Below port no will be moved and read from config folder later on
 
-// Below lines will be uncommented after route files are complete
-// currently route file(s) are empty!
-// load the routes for user
-//require("./app/routes/user.routes")(app);
-
-
-// set port and listen for requests, something like this :
-const PORT = process.env.PORT || 8085;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+server.listen(9000, () => {
+    console.log('server started on port 9000');
 });
