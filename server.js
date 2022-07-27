fedded3891d6994160f35cce98b2e0af08521247
Ravi Prakash <65698776+MallaRaviPrakashReddy@users.noreply.github.com>
2022-07-27 23:48:23 +0530

    checkpoint 10 final

--- server.js
+++ server.js
@@ -37,12 +37,20 @@ require("./app/routes/genre.routes")(app);
 // load the routes for artist
 require("./app/routes/artist.routes")(app);
 
+// load the routes for users
 require("./app/routes/user.routes")(app);
 
 // set up a default route for / 
 app.get("/", (req, res) => {
   res.json({ message: "Movie booking application" });
 });
+
+
+// Below lines will be uncommented after route files are complete
+// currently route file(s) are empty!
+// load the routes for user
+//require("./app/routes/user.routes")(app);
+
 
 // set port and listen for requests, something like this :
 const PORT = process.env.PORT || 8085;
