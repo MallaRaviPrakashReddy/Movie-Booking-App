ccbdd50244b9ecc6280422c125a0a19442d9f603
Ravi Prakash <65698776+MallaRaviPrakashReddy@users.noreply.github.com>
2022-07-27 23:47:14 +0530

    checkpoint 7

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 
 // make express object 
 const app = express();
+app.use(express.json());
 
 // use the CORS object 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -36,20 +37,15 @@ require("./app/routes/genre.routes")(app);
 // load the routes for artist
 require("./app/routes/artist.routes")(app);
 
+require("./app/routes/user.routes")(app);
+
 // set up a default route for / 
 app.get("/", (req, res) => {
   res.json({ message: "Movie booking application" });
 });
 
-
-// Below lines will be uncommented after route files are complete
-// currently route file(s) are empty!
-// load the routes for user
-//require("./app/routes/user.routes")(app);
-
-
 // set port and listen for requests, something like this :
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
